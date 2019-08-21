baa3742f495d54c30804f491c2898250fb5d3500
GuilhermeMLS <guitaikion@gmail.com>
2019-08-21 18:55:06 -0300

    Primeiro commit

---
+++ replacement/cmake-build-debug/CMakeFiles/gauss_elimination.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gauss_elimination.dir/main.c.o"
+  "CMakeFiles/gauss_elimination.dir/matrix.c.o"
+  "gauss_elimination.pdb"
+  "gauss_elimination"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gauss_elimination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ replacement/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "stdlib.h"
+
+#include "matrix.h"
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        printf("\nInvalid parameters\n");
+        return 0;
+    }
+
+    printf("\n## Solving Linear Systems with Replacement Technique ##\n");
+    int n = atof(argv[1]);
+    float *b = malloc(sizeof(float) * n);
+
+    float *array_result = malloc(sizeof(float) * n);
+
+    // Allocate matrix
+    float **matrix = allocateMatrix(n);
+
+    printf("\nN = %s\n", argv[1]);
+
+    printf("\nLoading matrix A\n");
+    loadMatrix(matrix, n);
+
+    printMatrix(matrix, n);
+
+    printf("\nLoading matrix B\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%f", &b[i]);
+    }
+
+    printArray(b, n);
+
+    replacementTechnique(matrix, b, array_result, n);
+
+    printf("\nResult matrix:\n");
+    printArray(array_result, n);
+
+    return 0;
+}
+
+
